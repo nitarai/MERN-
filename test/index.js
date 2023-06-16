42d5c44fd567ea03843f89cfd5887ee66cfd2aed
Nita Rai <rnita0891@gamil.com>
2023-06-16 15:11:13 +0545

    first class

--- test/index.js
+++ test/index.js
@@ -17,6 +17,62 @@ alert("hello nita");
 const age = prompt("how old are you?");
 console.log(age);
 
+alert (" how old are you?");
 const isBoss = confirm("are you boss?");
 console.log(isBoss);
 
+let x = 1;
+let y = 2;
+if (x==1){
+    console.log("x is 1");
+}
+if ( x===1 && y === 2)
+{
+    console.log("&& operator tested");
+}
+if ( x===1 || y === 3)
+{
+    console.log("|| operator tested");
+}
+
+if (!x)
+{
+    console.log("x is not true");
+}
+
+if (x === 1)
+{
+    console.log(" x is 1");
+} else{
+    console.log("x is not 1");
+}
+let result = x ===1 ? "x is 1" : " x is not 1";
+
+let results = x ===1 ? "x is 1" : x === 2 ? " x is 2": "x is not 1 and 2";
+
+for (let i = 0; i<10; i++)
+{
+    console.log(i);
+}
+
+//learning about functions
+
+//ES5 style
+function add (a,b)
+{
+    return a+b;
+    console.log(a+b);
+}
+
+// ES6 style (arrow function)
+const add =(a,b) => {
+    console.log(a+b);
+}
+
+function area(a,b)
+{
+    return a*b;
+}
+
+add (1,2);
+area (2,3);
