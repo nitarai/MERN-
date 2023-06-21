77b829ad23c30e822d4894ee12b7be2e39ea39a6
Nita Rai <rnita0891@gamil.com>
2023-06-21 14:40:46 +0545

    how to use npm

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 
 const http = require('http');
 const currentDateTime = require("./date.js");
+const uc = require('upper-case');
 http
     .createServer((req,res) => {
-          res.end('hello world!' + currentDateTime.myDateTime());
+        const message = uc.upperCase('hello world!');
+          res.end(message + currentDateTime.myDateTime());
         })
         .listen(8080);
         console.log('application running on port 8080');
